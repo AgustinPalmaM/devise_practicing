8930c9e86e9116ceb9867bf2c054c7b59ad19ceb
AgustinPalmaM <palma.agustin7@gmail.com>
2023-07-20 14:04:13 -0400

    Practicing devise, add feature to let user make sign_in using his username or email as login attribute

---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  attr_writer :login
+
+  validate :validate_username
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value",
+                                    { value: login.downcase }]).first
+    elsif conditions.key?(:username) || conditions.key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
+  def validate_username
+    return unless User.exists?(email: username)
+
+    errors.add(:username, :invalid)
+  end
+
+  def login
+    @login || username || email
+  end
+end
